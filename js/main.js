e0ece1402f3462ac46c79b25ef8c5fdce0fb57e1
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-06-10 15:23:05 -0300

    readyState

--- js/main.js
+++ js/main.js
@@ -2,11 +2,29 @@
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
-
+/* Open Cart */
 cartIcon.onclick = () => {
     cart.classList.add('active');
 };
-
+/* Close Cart */
 closeCart.onclick = () => {
     cart.classList.remove('active');
 };
+
+/* Cart Working JS */
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+}
+else {
+    ready();
+}
+
+/* Remove items from cart */
+function ready() {
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons);
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[1];
+        button.addEventListener('click', removeCartItem)
+    }
+}
