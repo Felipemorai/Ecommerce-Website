f2d7194f8cc2d0be672818d8df73d7f1968a0ac9
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-06-10 14:51:42 -0300

    cart.remove

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* Cart */
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () => {
+    cart.classList.add('active');
+};
+
+closeCart.onclick = () => {
+    cart.classList.remove('active');
+};
