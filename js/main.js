da7dd23ac0bec2f9c434d4a21308067f296db8b1
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-06-13 16:51:01 -0300

    updatetotal

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@ else {
 
 /* Remove Items From Cart */
 function ready() {
-    var removeCartButtons = document.getElementsByClassName('cart-remove')
-    console.log(removeCartButtons);
-    for (var i = 0; i < removeCartButtons.length; i++) {
-        var button = removeCartButtons[1]
+    var reomveCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(reomveCartButtons);
+    for (var i = 0; i < reomveCartButtons.length; i++) {
+        var button = reomveCartButtons[i]
         button.addEventListener('click', removeCartItem)
     }
 }
@@ -33,4 +33,17 @@ function ready() {
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.remove();
+}
+
+/* Update Total */
+function updatetotal() {
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    var cartBoxes = cartContent.getElementsByClassName('cart-box')
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i]
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
+        var quantity = quantityElement.value
+        total = total + price * quantity;
+    }
 }
