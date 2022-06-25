144abd800591209e1c13c89e882cd783b1a858a6
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-06-25 09:53:51 -0300

    buybutton

--- js/main.js
+++ js/main.js
@@ -39,6 +39,17 @@ function ready() {
         var button = addCart[i];
         button.addEventListener('click', addCartClicked);
     }
+    /* Buy Button Work */
+    document.getElementsByClassName('btn-buy')[0].addEventListener('click', buyButtonClicked);
+}
+/* Buy Button */
+function buyButtonClicked() {
+    alert('Your Order is placed');
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
 }
 
 /* Remove Items From Cart */
@@ -82,12 +93,12 @@ function addProductToCart(title, price, productImg) {
     }
 
 var cartBoxContent = `
-<img src="img/product2.jpg" alt="" class="cart-img">
+<img src="${productImg}" alt="" class="cart-img">
 <div class="detail-box">
     <div class="cart-product-title">
-        Earbuds
+        ${title}
     </div>
-    <div class="cart-price">$24.99</div>
+    <div class="cart-price">${price}</div>
     <input type="number" value="1" class="cart-quantity">
 </div>
 <i class="bx bxs-trash-alt cart-remove"></i>`;
@@ -110,9 +121,9 @@ function updatetotal() {
         var price = parseFloat(priceElement.innerText.replace("$", ""));
         var quantity = quantityElement.value;
         total = total + (price * quantity);
+    }
         /* Rounding Numbers Values */
         total = Math.round(total * 100) / 100;
 
         document.getElementsByClassName('total-price')[0].innerText = "$" + total;
-    }
 }
